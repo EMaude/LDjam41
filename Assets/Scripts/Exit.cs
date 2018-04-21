d2a2626607c818b65f07b785938cd8d4e5222ddd
Elliot Maude <maude.elliot@gmail.com>
2018-04-21 17:10:24 -0400

    3

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            if(nextIndex >= SceneManager.sceneCountInBuildSettings)
+            {
+                nextIndex = 0;
+            }
+
+            SceneManager.LoadScene(nextIndex);
+        }
+    }
+}
